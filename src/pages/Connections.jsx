8a6af20166bfdbac54bf9a273892d0a1061cab33
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2025-03-06 23:35:24 +0530

    worked on rendering & showing each connection in UI

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -1,29 +1,55 @@
-
-import { useEffect } from 'react';
-import { BASE_URL } from '../utils/constants'
 import axios from 'axios';
-//import { useDispatch } from 'react-redux';
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { BASE_URL } from '../utils/constants';
+import { addConnections } from '../utils/connectionsSlice';
 
 const Connections = () => {
-    //const dispatch = useDispatch();
 
+    const connections = useSelector((store) => store.addConnections);
+    const dispatch = useDispatch();
+    
     const fetchConnections = async () => {
         try {
             const res = await axios.get(BASE_URL + "/user/connections", {
                 withCredentials: true,
             });
-            console.log("connections for the loggedin user is:", res.data.data); 
-            //dispatch()
+            console.log("connections for the loggedin user is:", res.data); 
+            dispatch(addConnections(res.data.data))
         } catch (err) {
             console.log("Error is:", err);
         }
-    }
+    };
 
     useEffect(() => {
         fetchConnections();
     }, [])
+
+    if (!connections) return;
+    if (connections.length === 0) return <h1>No Connections Found</h1>
+
   return (
-    <div>Connections</div>
+    <>
+        <h1 className='text-center text-2xl'>Connections</h1>
+        {connections.map((connection) => (
+            <>
+                <div className="card card-side bg-base-100 shadow-sm" key={connection.id}>
+                    <figure>
+                        <img
+                        src={connection.photoURL}
+                        alt={connection.firstName} />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{connection.firstName}</h2>
+                        <p>{connection.age}</p>
+                        <p>{connection.gender}</p>
+                        <p>{connection.about}</p>
+                    </div>
+                </div>
+            </>
+        )
+        )}
+    </>
   )
 }
 
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
+import connectionsReducer from "../pages/Connections";
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
         feed: feedReducer,
+        connections: connectionsReducer,
     },
 });
 

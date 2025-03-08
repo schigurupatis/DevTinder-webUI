127df04b29048708f0faa3ad46ef9c38f2529507
Santha Kumar Chigurupati <schigurupatis@gmail.com>
2025-03-08 13:02:43 +0530

    worked on Connections component design & its rendering

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -1,57 +1,61 @@
-import axios from 'axios';
-import { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { BASE_URL } from '../utils/constants';
-import { addConnections } from '../utils/connectionsSlice';
+
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionsSlice";
 
 const Connections = () => {
+  const connections = useSelector((store) => store.connections);
+  const dispatch = useDispatch();
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res.data.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    const connections = useSelector((store) => store.addConnections);
-    const dispatch = useDispatch();
-    
-    const fetchConnections = async () => {
-        try {
-            const res = await axios.get(BASE_URL + "/user/connections", {
-                withCredentials: true,
-            });
-            console.log("connections for the loggedin user is:", res.data); 
-            dispatch(addConnections(res.data.data))
-        } catch (err) {
-            console.log("Error is:", err);
-        }
-    };
+  useEffect(() => {
+    fetchConnections();
+  }, []);
 
-    useEffect(() => {
-        fetchConnections();
-    }, [])
+  if (!connections) return;
 
-    if (!connections) return;
-    if (connections.length === 0) return <h1>No Connections Found</h1>
+  if (connections.length === 0) return <h1> No Connections Found</h1>;
 
   return (
-    <>
-        <h1 className='text-center text-2xl'>Connections</h1>
-        {connections.map((connection) => {
-            const {firstName, lastName, about, photoURL, age, gender} = connection;
-            return (
-                <div className="card card-side bg-base-100 shadow-sm" key={connection.id}>
-                    <figure>
-                        <img
-                        src={photoURL}
-                        alt={firstName + " " + lastName} />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{firstName}</h2>
-                        <p>{age}</p>
-                        <p>{gender}</p>
-                        <p>{about}</p>
-                    </div>
-                </div>
-            )
-        }
-        )}
-    </>
-  )
-}
+    <div className="text-center my-20">
+      <h1 className="text-bold text-white text-3xl mb-5">Connections</h1>
+      <div className="w-1/2 m-auto">
+      {connections.map((connection) => {
+        const { _id, firstName, lastName, photoURL, age, gender, about } =
+          connection;
 
-export default Connections
+        return (
+          <div className="card card-side bg-base-300 shadow-md p-2 my-3 flex justify-start items-center" key={_id}>
+            <figure className="c-p-img">
+              <img
+                src={photoURL}
+                alt={firstName}
+                className="w-24 h-24  rounded-full"
+
+              />
+            </figure>
+            <div className="card-body text-left p-0 ps-5">
+              <h2 className="card-title">{firstName + " " + lastName} </h2>
+              <h6>{gender + " " + age} </h6>
+              <p>{about}</p>
+            </div>
+          </div>
+        );
+      })}
+      </div>
+    </div>
+
+  );
+};
+export default Connections;
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedReducer from "./feedSlice";
-import connectionsReducer from "../pages/Connections";
+import connectionsReducer from "./connectionsSlice"
 
 const appStore = configureStore({
     reducer: {
